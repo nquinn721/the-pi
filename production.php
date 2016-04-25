30d98dd85747eb8595b06a05a965b111dea8b6e6
Nathan <natethepcspecialist@gmail.com>
2016-04-25 18:05:47 -0400

    update

--- production.php
+++ production.php
@@ -9,7 +9,7 @@ $db = new PDO('mysql:host=127.0.0.1;dbname=collabkings', 'root', '');
 
  <link rel="stylesheet" href="public/css/bootstrap.min.css">
  <link rel="stylesheet" href="public/css/main.css">
-
+Production
  <?php
  if ($handle = opendir('.')) {
 
