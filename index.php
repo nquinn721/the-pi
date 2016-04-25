ad481ab0cacd658a42645a589ac8a49f4f690dc4
Nathan <natethepcspecialist@gmail.com>
2016-04-25 18:16:14 -0400

    update

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php
-if($_SERVER['HTTP_HOST'] === 'localhost')
-	include 'development.php';
-else include 'production.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CollabKings</title>
+	<link rel="stylesheet" href="public/css/bootstrap.min.css">
+	<link rel="stylesheet" href="public/css/main.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+</head>
+<body>
+<?php include 'lib/nav.php';?>
+<div class="container">
+	<?php
+	if($_SERVER['HTTP_HOST'] === 'localhost')
+		include 'development.php';
+	else include 'production.php';
+	?>
+</div>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</body>
+</html>
--- production.php
+++ production.php
@@ -1,20 +1,17 @@
-
 <?php
 try{
-$db = new PDO('mysql:host=127.0.0.1;dbname=collabkings', 'root', '');
+	$db = new PDO('mysql:host=127.0.0.1;dbname=collabkings', 'root', '');
 }catch(PDOException $e){
- echo 'PDO ERROR: ' . $e->getMessage();
- }
+ 	echo 'PDO ERROR: ' . $e->getMessage();
+}
  ?>
 
- <link rel="stylesheet" href="public/css/bootstrap.min.css">
- <link rel="stylesheet" href="public/css/main.css">
+
  <?php
  if ($handle = opendir('.')) {
 
-    /* This is the correct way to loop over the directory. */
 	while (false !== ($entry = readdir($handle))) {
-	     if(strpos($entry, '.') === false && $entry !== 'public'){
+	     if(strpos($entry, '.') === false && $entry !== 'public' && $entry !== 'lib'){
          	echo "<div class='col-md-6 site-container'><a href='$entry' class='site-link'>$entry</a></div>";
          }
 	}
