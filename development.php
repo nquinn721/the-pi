410413376abb694fa9e8022d7046ac52036fec1b
Nathan <natethepcspecialist@gmail.com>
2016-04-26 19:20:11 -0400

    'update'

--- development.php
+++ development.php
@@ -1,4 +1,10 @@
- 
+<?php
+try{
+	$db = new PDO('mysql:host=127.0.0.1;dbname=coolhax', 'root', '');
+}catch(PDOException $e){
+ 	echo 'PDO ERROR: ' . $e->getMessage();
+}
+ ?> 
 <div class='col-md-6 site-container'>
 	<a href='donze-site' class='site-link'>donze-site</a>
 </div>
